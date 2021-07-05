a70663ba72de810846e86bb6e2e638179cc5cada
nath248 <nmherrera20@outlook.com>
2021-07-05 16:10:00 -0400

    Add object for player details

--- script.js
+++ script.js
@@ -1,31 +1,46 @@
-let firstCard = getRandomCard();
-let secondCard = getRandomCard();
-let cards = [firstCard, secondCard]
-let sum = firstCard + secondCard;
+let cards = [];
+let sum = 0;
 let hasBlackjack = false;
-let isAlive = true;
+let isAlive = false;
 let message = "";
-
 let messageEl = document.getElementById("message-el");
 let sumEl = document.getElementById("sum-el");
 let cardsEl = document.getElementById("cards-el");
+let player = {
+    name: "Nathaly",
+    chips: 145
+};
+
+let playerEl = document.getElementById("player-el");
+playerEl.textContent = player.name + ": $" + player.chips;
 
 function getRandomCard() {
     let randomNumber = Math.floor(Math.random() * 13) + 1;
-    return randomNumber;
+    if (randomNumber === 1) {
+        return 11;
+    }
+    if (randomNumber > 10) {
+        return 10;
+    } else {
+        return randomNumber;
+    };
 };
 
 function startGame() {
+    isAlive = true;
+    let firstCard = getRandomCard();
+    let secondCard = getRandomCard();
+    cards = [firstCard, secondCard];
+    sum = firstCard + secondCard;
     renderGame();
 };
 
 function renderGame() {
-    sumEl.textContent = "Sum: " + sum;
     cardsEl.textContent = "Cards: ";
     for (let i = 0; i < cards.length; i++) {
         cardsEl.textContent += cards[i] + " ";
     };
-
+    sumEl.textContent = "Sum: " + sum;
     if (sum <= 20) {
         message = "Would you like to draw another card?";
     } else if (sum === 21) {
@@ -39,9 +54,10 @@ function renderGame() {
 };
 
 function newCard() {
-    let card = getRandomCard();
-    sum += card;
-    cards.push(card);
-    console.log(cards);
-    renderGame();
-}
+    if (isAlive === true && hasBlackjack === false) {
+        let card = getRandomCard();
+        sum += card;
+        cards.push(card);
+        renderGame();
+    }
+};
